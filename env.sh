3b776aada33ec59b870e57249686c0eb2cecc96e
xueshuming <xueshuming@minigui.org>
2020-12-12 13:37:37 +0800

    update CMakeLists.txt

--- env.sh
+++ env.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-BASE_DIR=$(dirname $(readlink -f "$0"))
-SOURCE_DIR="$BASE_DIR/../../Source/"
-WEBKIT_OUTPUTDIR="$BASE_DIR/../../Output/HBD/"
+export HYBRIDOS_OUTPUT_ROOT="/home/xue/work/hybridos/device-side/Output/HBD/Root/"
+export CMAKE_INCLUDE_PATH="$HYBRIDOS_OUTPUT_ROOT/include"
+export CMAKE_LIBRARY_PATH="$HYBRIDOS_OUTPUT_ROOT/lib"
+export PKG_CONFIG_PATH="$HYBRIDOS_OUTPUT_ROOT/lib/pkgconfig"
 
 # LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$WEBKIT_OUTPUTDIR/Root/lib/:$LD_LIBRARY_PATH 
+export LD_LIBRARY_PATH=$HYBRIDOS_OUTPUT_ROOT/Root/lib/:$HYBRIDOS_OUTPUT_ROOT/Root/usr/lib/$LD_LIBRARY_PATH 
 
 # mgncs.cfg
-export NCS_CFG_PATH=$WEBKIT_OUTPUTDIR/Root/etc/
+export NCS_CFG_PATH=$HYBRIDOS_OUTPUTDIR/Root/etc/
