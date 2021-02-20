3111e1adcbda3d885b4f46ec4057ca400853e784
gengyue <gengyue@minigui.org>
2021-02-20 15:11:49 +0800

    modified appagent cmakelists.txt

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export HYBRIDOS_OUTPUT_ROOT="/home/xue/work/hybridos/device-side/Output/HBD/Root/"
+export HYBRIDOS_OUTPUT_ROOT="/home/projects/hybridos/device-side/Output/HBD/Root/"
 export CMAKE_INCLUDE_PATH="$HYBRIDOS_OUTPUT_ROOT/include"
 export CMAKE_LIBRARY_PATH="$HYBRIDOS_OUTPUT_ROOT/lib"
 export PKG_CONFIG_PATH="$HYBRIDOS_OUTPUT_ROOT/lib/pkgconfig"
 
 # LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$HYBRIDOS_OUTPUT_ROOT/Root/lib/:$HYBRIDOS_OUTPUT_ROOT/Root/usr/lib/$LD_LIBRARY_PATH 
+export LD_LIBRARY_PATH=$HYBRIDOS_OUTPUT_ROOT/lib/:$HYBRIDOS_OUTPUT_ROOT/usr/lib/$LD_LIBRARY_PATH 
 
 # mgncs.cfg
 export NCS_CFG_PATH=$HYBRIDOS_OUTPUTDIR/Root/etc/
